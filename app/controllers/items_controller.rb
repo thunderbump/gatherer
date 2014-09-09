61cff4d10eb034ebca9ed801f5f275b370bcd0ed
bump <thunderbump@gmail.com>
2014-09-09 11:14:58 -0700

    initial import. basic item functionality included

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,39 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to items_path
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to items_path
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :url)
+  end
+end
